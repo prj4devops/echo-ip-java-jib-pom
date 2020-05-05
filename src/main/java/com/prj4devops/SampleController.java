6650feb9b744d4f33aac1abbb93c9d37885921ec
GNU Shim <geunwoo.j.shim@gmail.com>
2020-05-06 00:24:13 +0900

    Update SampleController.java

--- src/main/java/com/prj4devops/SampleController.java
+++ src/main/java/com/prj4devops/SampleController.java
@@ -12,7 +12,7 @@ public class SampleController {
 
     @RequestMapping("/")
     public String hello(HttpServletRequest request){
-        String result = "src: "+request.getRemoteAddr()+" / dest"+request.getServerName()+"\n";
+        String result = "src: "+request.getRemoteAddr()+" / dest: "+request.getServerName()+"\n";
         return result;
     }
 }
